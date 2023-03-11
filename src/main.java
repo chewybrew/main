805db19a04668d4cd15c4cebc3b7a25907eb36d4
chewybrew <ihatepeoqle@gmail.com>
2023-03-11 19:36:51 +0500

    commit 2

--- src/main.java
+++ src/main.java
@@ -1,7 +1,40 @@
-public class main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-        System.out.println("a");
+public class main {
+    public static void main(String[] args) throws IOException {
+
+        String url = "https://www.gate.io/ru/trade/RVN_USDT";
+
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while (true) {
+            try {
+                if (!((inputLine = in.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            response.append(inputLine);
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String str2 = response.substring(0, 500);
+        System.out.println(str2.substring(str2.indexOf("<title>")+7, str2.indexOf("</title>")-38));
 
     }
 }
